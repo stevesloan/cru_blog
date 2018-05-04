98abf05aba37bbb05f3b271914868d37efdea830
Steve Sloan <steve@stevesloan.ca>
2018-05-04 11:31:03 -0400

    Dockerfile: initialize database with test data

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,9 @@ RUN apt-get update \
   && docker-php-ext-install pdo_mysql \
   && apt-get clean \
   && rm -rf /var/cache/apt/archives
-
-COPY . /var/www/
-COPY index.php /var/www/html/
-WORKDIR /var/www/blog
 EXPOSE 8080
 
+COPY . /var/www/
+COPY blog/.env.docker /var/www/blog/.env
+WORKDIR /var/www/blog
 ENTRYPOINT ./init.sh &&  php artisan serve --port 8080 --host 0.0.0.0
--- blog/init.sh
+++ blog/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 if [ ! -f ./.INSTALLED ]; then
-    sleep 20
+    until mysql -h db -P 3306 -uroot -pexample; do
+        sleep 1
+        echo "MySql is unavailable - sleeping"
+    done
+    echo "MySql is available"
     mysql -h db -P 3306 -uroot -pexample -e "CREATE DATABASE IF NOT EXISTS crublog"
     php artisan make:model BlogPost -m && php artisan migrate
+    mysql -h db -uroot -pexample crublog < test_db_2017-05-24.sql
     touch .INSTALLED
 fi
