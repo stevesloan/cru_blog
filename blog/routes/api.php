6068bdb8385078857b018a6776673eb3c0cf8edf
Steve Sloan <steve@stevesloan.ca>
2018-05-04 00:16:38 -0400

    api: setup read endpoints

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
-
+Use App\User;
+Use App\UserRole;
+Use App\BlogPost;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +18,44 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// users
+Route::get('users', function() {
+    $arUsers = User::all();
+    foreach ($arUsers as $user) {
+        $user->role = $user->role;
+        $user->post_count = sizeof($user->blogPosts);
+        $user->address = $user->address;
+    }
+    return $arUsers;
+});
+
+Route::get('users/{id}', function($id) {
+    $user = User::find($id);
+    $user->role = $user->role;
+    $user->post_count = sizeof($user->blogPosts);
+    $user->address = $user->address;
+    return $user;
+});
+
+// blog_posts
+Route::get('blog_posts', function() {
+    return BlogPost::all();
+});
+
+Route::get('blog_posts/user/{id}', function($id) {
+    return User::find($id)->blogPosts;
+});
+
+Route::get('blog_posts/{id}', function($id) {
+    return BlogPost::find($id);
+});
+
+
+
+// /create_blog_post
+// params: (whatever is needed to create a new blog post)
+//
+// /edit_user
+// edit a user’s details, including their role and address
+// params: (whatever is needed to accomplish this)
